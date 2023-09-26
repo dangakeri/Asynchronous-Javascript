a3868e3d13229ebc15383bffca0c930e1ca5fc07
dangakeri <dangakeri254@gmail.com>
2023-09-26 07:50:32 +0300

    running promises in parallel

--- starter/script.js
+++ starter/script.js
@@ -344,9 +344,55 @@ const whereAmI = async function () {
   } catch (err) {
     console.error(err);
     renderError(`Something went wrong ${err.message}`);
+
+    // reject promise returned from async function
+    throw err;
   }
 };
 
-console.log('1: Will get location');
-whereAmI();
-console.log('2: finished getting location');
+// console.log('1: Will get location');
+
+// whereAmI()
+//   .then(city => console.log(`2: ${city}`))
+//   .catch(err => console.error(`2: ${err.message}`))
+//   .finally(() => console.log('2: finished getting location'));
+
+(async function () {
+  try {
+    const city = await whereAmI();
+    console.log(`2: ${city}`);
+  } catch (error) {
+    console.error(`2: ${error.message}`);
+  }
+  console.log('2: finished getting location');
+})();
+
+const getJSON = function (url, errMes = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errMes} (${response.status})`);
+    return response.json();
+  });
+};
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    /*
+    // Making API calls in series
+    const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    console.log(data1.capital, data2.capital, data3.capital);
+    */
+
+    // Making API calls in parrarel
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (error) {
+    console.log(error);
+  }
+};
+get3Countries('kenya', 'canada', 'tanzania');
