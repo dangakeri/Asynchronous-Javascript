0be6eb0a7512a326482387849d51045e79871499
dangakeri <dangakeri254@gmail.com>
2023-09-26 08:30:33 +0300

    promise combinators

--- starter/script.js
+++ starter/script.js
@@ -357,15 +357,15 @@ const whereAmI = async function () {
 //   .catch(err => console.error(`2: ${err.message}`))
 //   .finally(() => console.log('2: finished getting location'));
 
-(async function () {
-  try {
-    const city = await whereAmI();
-    console.log(`2: ${city}`);
-  } catch (error) {
-    console.error(`2: ${error.message}`);
-  }
-  console.log('2: finished getting location');
-})();
+// (async function () {
+//   try {
+//     const city = await whereAmI();
+//     console.log(`2: ${city}`);
+//   } catch (error) {
+//     console.error(`2: ${error.message}`);
+//   }
+//   console.log('2: finished getting location');
+// })();
 
 const getJSON = function (url, errMes = 'Something went wrong') {
   return fetch(url).then(response => {
@@ -373,26 +373,75 @@ const getJSON = function (url, errMes = 'Something went wrong') {
     return response.json();
   });
 };
-const get3Countries = async function (c1, c2, c3) {
-  try {
-    /*
-    // Making API calls in series
-    const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
-    const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
-    const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+// const get3Countries = async function (c1, c2, c3) {
+//   try {
+//     /*
+//     // Making API calls in series
+//     const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+//     const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+//     const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
 
-    console.log(data1.capital, data2.capital, data3.capital);
-    */
+//     console.log(data1.capital, data2.capital, data3.capital);
+//     */
 
-    // Making API calls in parrarel
-    const data = await Promise.all([
-      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
-      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
-    ]);
-    console.log(data.map(d => d[0].capital));
-  } catch (error) {
-    console.log(error);
-  }
+//     // Making API calls in parrarel
+//     const data = await Promise.all([
+//       getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+//       getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+//     ]);
+//     console.log(data.map(d => d[0].capital));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+// get3Countries('kenya', 'canada', 'tanzania');
+
+// Promise.race
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/egypt`),
+    getJSON(`https://restcountries.com/v3.1/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('Request took too long'));
+    }, sec * 1000);
+  });
 };
-get3Countries('kenya', 'canada', 'tanzania');
+
+Promise.race([
+  getJSON(`https://restcountries.com/v3.1/name/tanzania`),
+  timeout(0.01),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise.allSettled
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another Success'),
+]).then(res => console.log(res[0]));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another Success'),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// Promise any
+Promise.any([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another Success'),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
